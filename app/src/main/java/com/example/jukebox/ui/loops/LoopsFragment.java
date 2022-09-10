7009b4bed133fa1a2151bc51462a3130e9b74b2b
eduardotorresferreira <etf1974@hotmail.com>
2022-09-10 16:23:46 -0400

    added spinner

--- app/src/main/java/com/example/jukebox/ui/loops/LoopsFragment.java
+++ app/src/main/java/com/example/jukebox/ui/loops/LoopsFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.jukebox.R;
 import com.example.jukebox.databinding.FragmentLoopsBinding;
 
 public class LoopsFragment extends Fragment {
@@ -15,12 +18,30 @@ public class LoopsFragment extends Fragment {
     private FragmentLoopsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         binding = FragmentLoopsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textDashboard;
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupSpinner();
+        setupListView();
+    }
+
+    public void setupSpinner(){
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                requireContext(), R.array.instruments_array,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerInstrument.setAdapter(adapter);
+    }
+
+    private void setupListView(){
+
+        //binding.listViewInstrument.setAdapter();
     }
 
     @Override
